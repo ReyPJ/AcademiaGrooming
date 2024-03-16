29a297e1c9976b37b62148e25c4fd679a0238f72
Andrew27mT11 <andresteven2711@gmail.com>
2024-03-16 17:18:34 -0600

    Union header y footer
    Se arreglaron unas cosas

--- VbAcademy/core/static/core/scripts/headerScript.js
+++ VbAcademy/core/static/core/scripts/headerScript.js
@@ -10,23 +10,11 @@ function hoverMenuIcon(enter) {
 }
 
 function toggleMenu() {
-    var hiddenButtons = document.querySelectorAll('.menu__item.hidden');
-    hiddenButtons.forEach((button, index) => {
-        setTimeout(function () {
-            button.style.display = 'inline-block'; // Mostrar el botón
-            animateButton(button, index); // Animar el botón
-        }, (index + 1) * 200); // Ajuste para evitar superposiciones
-    });
-}
+    var menuItems = document.getElementById('menuItems');
 
-function animateButton(button, index) {
-    var menuButton = document.getElementById('menuButton');
-    var rectMenuButton = menuButton.getBoundingClientRect();
-
-    if (button.id !== 'menuButton') { // Ignorar el botón principal
-        var previousButton = document.querySelector('.menu__item.hidden:nth-child(' + index + ')'); // Botón anterior
-        var distance = menuButton.offsetWidth + (index * (previousButton.offsetWidth + 20)); // Calcular la distancia de desplazamiento correctamente
-        button.style.transition = 'transform 0.5s ease'; // Duración y tipo de la animación
-        button.style.transform = 'translateX(-' + distance + 'px)'; // Desplazar el botón hacia la izquierda
+    if (menuItems.style.display === 'none' || menuItems.style.display === '') {
+        menuItems.style.display = 'block'; // Mostrar el menú
+    } else {
+        menuItems.style.display = 'none'; // Ocultar el menú
     }
 }
