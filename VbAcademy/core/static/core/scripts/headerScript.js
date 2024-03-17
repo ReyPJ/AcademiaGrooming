6483e88421717727b857ee1dca8e33e376c62b6c
Andrew27mT11 <andresteven2711@gmail.com>
2024-03-16 19:30:50 -0600

    Header Terminado

--- VbAcademy/core/static/core/scripts/headerScript.js
+++ VbAcademy/core/static/core/scripts/headerScript.js
@@ -13,8 +13,22 @@ function toggleMenu() {
     var menuItems = document.getElementById('menuItems');
 
     if (menuItems.style.display === 'none' || menuItems.style.display === '') {
-        menuItems.style.display = 'block'; // Mostrar el menú
+        menuItems.style.display = 'flex'; // Cambia la visualización a flex
+        var buttons = document.querySelectorAll('.menu__item');
+        buttons.forEach((button, index) => {
+            setTimeout(function () {
+                button.style.opacity = '2'; // Muestra los botones con retraso
+                button.style.transform = 'translateX(0)'; // Mueve los botones a su posición original
+            }, index * 150); // Añade un retraso para crear el efecto de desplazamiento secuencial
+        });
     } else {
-        menuItems.style.display = 'none'; // Ocultar el menú
+        var buttons = document.querySelectorAll('.menu__item');
+        buttons.forEach((button, index) => {
+            button.style.opacity = '0'; // Oculta los botones
+            button.style.transform = 'translateX(100%)'; // Mueve los botones fuera de la pantalla hacia la derecha
+        });
+        setTimeout(function () {
+            menuItems.style.display = 'none'; // Oculta el menú después de que los botones se desvanezcan
+        }, 300 + (buttons.length - 1) * 200); // Calcula el tiempo total de la animación
     }
 }
