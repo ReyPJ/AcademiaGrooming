06f996dd9c8c8cb99f8829ab97b2883ae949b85b
Reyner <reyenr1012002@gmail.com>
2024-03-16 23:57:14 -0600

    añadiendo redes dinamicos con ctx

--- VbAcademy/VbAcademy/settings.py
+++ VbAcademy/VbAcademy/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'ckeditor',
     "core.apps.CoreConfig",
+    "social.apps.SocialConfig"
 ]
 
 MIDDLEWARE = [
@@ -64,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social.processors.social_dict'
             ],
         },
     },
