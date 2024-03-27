1530d4f174cdc51dc74075e0ce6ef36f256c9234
Andrew27mT11 <andresteven2711@gmail.com>
2024-03-27 16:12:34 -0600

    Cards con BD
    Las cards de cursos se llenan dinamicamente con la base de datos y se creo el templatetag de curso

---
+++ VbAcademy/Cursos/templatetags/todosCursos.py
@@ -0,0 +1,9 @@
+from django import template
+from Cursos.models import Curso
+
+register = template.Library()
+
+@register.simple_tag()
+def get_all_cursos():
+    cursos = Curso.objects.all()
+    return cursos
